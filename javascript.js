ec2a6da9d5a9df93e627332b2ddde2dd38b734fd
Iraj-Aleem <128618337+Iraj-Aleem@users.noreply.github.com>
2024-08-15 20:18:44 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+<!DOCTYPE   html>
+<html>
+    <head>
+
+        </head>
+    <body>
+<script>
+   // function    myFunction(){
+document.getElementById("demo").innerHTML="Hello JavaScript";
+document.write("technologiesis  text");
+window.alert("Transaction   doesnsucessfully");
+console.log("submitofTwo    numbers");
+Window.print("Print Form");
+ //   }
+</script>
+    </body>
+</html>
